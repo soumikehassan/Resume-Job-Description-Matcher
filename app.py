dc665db287466e547aaf49c29c7f956ab171858d
soumikehassan <md.soumikehassan@gmail.com>
2025-05-10 18:28:50 +0600

    update app code

--- app.py
+++ app.py
@@ -33,6 +33,8 @@ def download_resources():
     nltk.download('punkt', quiet=True)
     nltk.download('stopwords', quiet=True)
     nltk.download('wordnet', quiet=True)
+    nltk.download('punkt_tab',quiet=True)
+    
 
     # Load spaCy model
     try:
